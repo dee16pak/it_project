c783aa4411d3bf9f05df0c4bdde8750f23a42aa5
Deepak Yadav <dyadav335@gmaill.com>
2018-10-06 00:03:30 +0530

    Checkout page updated

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CheckoutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CheckoutPage {
 
+ 
+ 
+ 
+ 
+  orderList: any[] = [];
+  total : any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    this.orderList = [
+      {cat : '1', name : 'Dal', sub_name: ['Half plate'], price: 120, qty : 2},
+      {cat : '1', name : 'Paneer', sub_name: ['Full plate'], price: 180, qty : 3},
+      {cat : '2', name : 'Roti', sub_name: ['atta'], price: 30, qty : 3}
+      
+    ];
+    this.total = 0.0;
+  }  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckoutPage');
+    for(let i = 0 ; i<this.orderList.length;i++){
+      this.total = (this.orderList[i].price * this.orderList[i].qty) + this.total;
+    }
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { CheckoutPage } from '../checkout/checkout';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +10,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  gotoCheckout(){
+    this.navCtrl.push(CheckoutPage)
+  }
 }
